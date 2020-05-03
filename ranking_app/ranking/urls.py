52fd3c7893c4b9128f7a989d8992379aa1c589dc
kyosuke <sspvt065@gmail.com>
2020-05-03 22:52:42 +0900

    ポイントの推移を表したグラフを追加し詳細ページに反映

--- ranking_app/ranking/urls.py
+++ ranking_app/ranking/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('category/<int:category_id>/', views.category_index, name='category'),
     path('content/<int:content_id>', views.content_detail,
          name='content_detail'),
+    path('content/<int:content_id>/rank_graph', views.get_svg,
+         name='content_rank_graph'),
 ]
